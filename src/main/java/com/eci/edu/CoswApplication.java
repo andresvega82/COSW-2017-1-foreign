7c04b7aaa6cad17c54067917803390b32bb6ccfa
nicomoregu <nicomoregu@gmail.com>
2017-03-17 17:25:04 -0500

    correccion seguridad

--- src/main/java/com/eci/edu/CoswApplication.java
+++ src/main/java/com/eci/edu/CoswApplication.java
@@ -6,8 +6,6 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 
