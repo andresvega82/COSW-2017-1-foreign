f6f42348d249bc555378343a1bd346820fecb06c
tatisandreea <tata_higuera@hotmail.com>
2017-03-28 18:14:29 -0500

    repositorios

--- src/main/java/com/eci/edu/repository/StudentRepository.java
+++ src/main/java/com/eci/edu/repository/StudentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 /**
  * Created by  on 19/03/17.
  */
-public interface StudentRepository{ //extends JpaRepository < Student, StudentId> {
+public interface StudentRepository extends JpaRepository < Student, StudentId> {
 
     /*@Query("from Students s where s.student_id= :userId ")
     public Student getClientePorId(@Param(value = "userId")String userId);
