b0757e235e2514250ddfa1580e42038c338c1d15
tatisandreea <tata_higuera@hotmail.com>
2017-03-29 01:04:14 -0500

    cambios controler tutorial

--- src/main/java/com/eci/edu/services/TutorialServicesImpl.java
+++ src/main/java/com/eci/edu/services/TutorialServicesImpl.java
@@ -6,6 +6,17 @@ import com.eci.edu.repository.TutorialRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.eci.edu.entities.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.hibernate.cfg.Configuration;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import java.util.List;
 
 /**
@@ -41,6 +52,40 @@ public class TutorialServicesImpl implements InterfaceTutorials{
 
     @Override
     public List<Teacher> getTutorialsByParams(String params){
+
+        SessionFactory sf = getSessionFactory();
+        Session s=sf.openSession();
+        Transaction tx=s.beginTransaction();
+
+
+
+        tx.commit();
+        s.close();
+        sf.close();
+
         return null;//tutorialRepository.getTutorialByParams(params);
+    }
+
+
+    public static SessionFactory getSessionFactory() {
+        // loads configuration and mappings
+        Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(CreditCard.class);
+        configuration.addAnnotatedClass(Languaje.class);
+        configuration.addAnnotatedClass(Student.class);
+        configuration.addAnnotatedClass(StudentId.class);
+
+        configuration.addAnnotatedClass(TeacherId.class);
+        configuration.addAnnotatedClass(Teacher.class);
+        configuration.addAnnotatedClass(Tutorial.class);
+
+        ServiceRegistry serviceRegistry
+                = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        // builds a session factory from the service registry
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        return sessionFactory;
     }
 }
