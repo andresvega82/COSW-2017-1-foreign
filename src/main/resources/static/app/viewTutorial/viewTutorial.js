5eeee345f81cad2759f2b1059507bc7e9191c288
tatisandreea <tata_higuera@hotmail.com>
2017-03-28 21:34:29 -0500

    cambios vistas agendar una tutoria

--- src/main/resources/static/app/viewTutorial/viewTutorial.js
+++ src/main/resources/static/app/viewTutorial/viewTutorial.js
@@ -11,48 +11,39 @@ angular.module('myApp.viewTutorial', ['ngRoute'])
             controller: 'ViewTutorialCtrl'
         });
     }])
-    .controller('ViewTutorialCtrl', ['$scope', '$rootScope','$http','$resource', 'tutorial', function($scope, $rootScope, $http, $resource, tutorial) {
+    .controller('ViewTutorialCtrl', ['$scope', '$rootScope','$http','$resource', 'tutores','tutorial', function($scope, $rootScope, $http, $resource, tutores, tutorial) {
         $scope.userId = null;
         $scope.newTutorial = false;
         $scope.tutorialId = null;
         $scope.languaje = "";
         $scope.date = "";
-        $scope.hour = "";
-        $scope.timeHour = null;
-        $scope.timeMin = null;
-        $scope.busy = false;
-        $scope.tutorialListAll = []
-        sessionStorage.setItem('lenguaje',$scope.languaje);
+        $scope.tutoresListAll = [];
+        $rootScope.id = 1;
 
-        $scope.agregarTutoria = function(){
+        $scope.buscarTutorias = function(){
             if ($scope.languaje!="") {
                 if ($scope.date!=null) {
-                    if ($scope.hour!=null) {
-                        if ($scope.timeHour!=null) {
-                            var newTutorial = {"languaje":$scope.languaje,"date":$rootScope.date,"hour":$scope.hour,"timeHour":$scope.timeHour,"timeMin":$scope.timeMin};
-                            console.log(newTutorial)
-                            console.log("creando nueva tutoria, datos iniciales")
-                            tutorial.save(newTutorial, function(){
-                                console.log("guardando la tutoria")
+                    var newTutorial = {"languaje":$scope.languaje,"date":$rootScope.date};
+                        console.log(newTutorial)
+                        console.log("Buscando una tutoria, datos iniciales")
+                        /*tutorial.save(newTutorial, function(){
+                            console.log("Buscando tutorias")
+                            });*/
 
 
-                            });
-                        }else{
-                            alert("Todos los campos son obligatorios, falta la Duracion de la sesion");
-                        }
-                    }else{
-                        alert("Todos los campos son obligatorios, falta hora inicial ");
-                    }
                 }else{
                     alert("Todos los campos son obligatorios, falta la fecha");
                 }
             }else{
                 alert("Todos los campos son obligatorios, falta el lenguaje");
-                console.log($scope.languaje)
             }
         }
 
 
 
 
-    }]);
+    }])
+
+
+
+    ;
